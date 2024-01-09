bfcd1bdf1501f4a241be80bc88a4a2990d5e8496
kerzers <ahra.ahlame@gmail.com>
2024-01-09 20:15:05 +0100

    task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,4 @@
 export default function getStudentIdsSum(listStudents) {
-    const sum = listStudents.reduce((accum, student) => {
-        return accum + student.id;
-    }, 0);
-    return sum;
+  const sum = listStudents.reduce((accum, student) => accum + student.id, 0);
+  return sum;
 }
