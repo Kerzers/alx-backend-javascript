4778925f7f2d320403855ce80dbaf0839a22878c
root <root@PC>
2024-01-09 13:34:20 +0100

    task 0&1

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,11 @@
+
+export default function getListStudentIds(array) {
+    let arrayIds;
+    if (Array.isArray(array)) {
+        arrayIds = array.map(obj => obj.id)
+    }
+    else {
+        arrayIds = [];
+    }
+    return arrayIds;
+}
